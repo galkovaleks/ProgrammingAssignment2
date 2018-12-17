ce6a5718980a86ed40eb5faba8507ee8d38202e7
galkovaleks <45938344+galkovaleks@users.noreply.github.com>
2018-12-17 17:27:57 +0300

    Update cachematrix.R
    working commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+initMatrixCache <- function(x = matrix()) {
+  
+	# 'initMatrixCache'. Метод создает кеширующую матрицу из созданной 
 
-## Write a short comment describing this function
+	myMatrix <- NULL	#инициализация метода 'myMatrix'    
 
-makeCacheMatrix <- function(x = matrix()) {
+	setMatrix <- function(y) {
+		x <<- y
+		myMatrix <<- NULL
+	}	#определение метода 'setMatrix'  
 
+	getMatrix <- function() x	#определение метода 'getMatrix'. Метод возвращает матрицу 'x'    
+
+	setCache <- function(inverse) myMatrix <<- inverse	#определение метода 'setCache'    
+
+	getCache <- function() myMatrix	#определение метода 'getCache'. Возвращает обратную матрицу 'x'    
+
+	list(setMatrix = setMatrix,
+		getMatrix = getMatrix,
+		setCache = setCache,
+		getCache = getCache)  	#список имен методов доступных извне
 }
+ #---------------------------------------------------------
 
 
-## Write a short comment describing this function
+inverceMatrixCache <- function(x, ...) {  
+  
+	# 'inverceMatrixCache'. Возвращает обратную матрицу  
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	myMatrix <- x$getCache()    #Проверка полученной матрицы  
+
+	if (!is.null(myMatrix)) {
+		message("loading cache matrix...")
+		return(myMatrix)
+	}	#Обработка не пустой матрицы   
+	else {
+		dMatrix <- x$getMatrix()
+		myMatrix <- solve(dMatrix, ...)
+		x$setCache(myMatrix)
+		return(myMatrix)
+	}  #Обработка случая пустой матрицы
 }
+#---------------------------------------------------------
